74a04732dbe7fea427a6427ab34ac1ec575653b1
Thomas Fruetel <github@fruetel.de>
2018-10-02 20:22:31 +0200

    Add empty map component

---
+++ src/components/map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Map = () => (
+  <>
+    <h1>You are here:</h1>
+  </>
+);
+
+export default Map;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Map from '../components/map';
 
 import Layout from '../components/layout';
 
@@ -7,6 +8,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Now go build something great.</p>
+    <Map />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
