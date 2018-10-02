c7b79d0a46cd492c5454ec1b8454c4a33c91754c
Thomas Fruetel <github@fruetel.de>
2018-10-02 20:34:31 +0200

    Add leaflet to map component

--- src/components/map.js
+++ src/components/map.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-const Map = () => (
+const position = [51.21435, 6.74935];
+const MapWrapper = () => (
   <>
     <h1>You are here:</h1>
+    <Map center={position} zoom={18}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup.
+          <br />
+          Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
   </>
 );
 
-export default Map;
+export default MapWrapper;
