87320574e7d8b24c71287b1cf0ddf61d6b44be07
Thomas Fruetel <github@fruetel.de>
2018-10-02 20:50:02 +0200

    Use leaflet plugin for Gatsby

--- src/components/map.js
+++ src/components/map.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import './map.css';
 
-const position = [51.21435, 6.74935];
-const MapWrapper = () => (
-  <>
-    <h1>You are here:</h1>
-    <Map center={position} zoom={18}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup.
-          <br />
-          Easily customizable.
-        </Popup>
-      </Marker>
-    </Map>
-  </>
-);
-
-export default MapWrapper;
+export default class MapWrapper extends Component {
+  render() {
+    const position = [51.21435, 6.74935];
+    if (typeof window !== 'undefined') {
+      return (
+        <>
+          <h1>You are here:</h1>
+          <Map center={position} zoom={18}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup.
+                <br />
+                Easily customizable.
+              </Popup>
+            </Marker>
+          </Map>
+        </>
+      );
+    }
+    return null;
+  }
+}
